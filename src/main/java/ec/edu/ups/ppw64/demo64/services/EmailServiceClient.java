6b3e6cd6039a22d56bf250022e20b28572eb7521
PaulSigua <mateosigua2002@gmail.com>
2024-07-04 17:18:49 -0500

    cambios

--- src/main/java/ec/edu/ups/ppw64/demo64/services/EmailServiceClient.java
+++ src/main/java/ec/edu/ups/ppw64/demo64/services/EmailServiceClient.java
@@ -10,7 +10,10 @@ import org.springframework.http.MediaType;
 
 public class EmailServiceClient {
 
-    private static final String SERVER_URL = "http://35.184.173.35:8080/mail/rs/email/enviar"; // Ajusta la URL y el puerto según tu configuración
+    // URLs de los servidores de correo
+    private static final String SERVER_URL_1 = "http://35.184.173.35:8080/mail/rs/email/enviar";
+    private static final String SERVER_URL_2 = "http://34.121.27.10:8080/mail/rs/email/enviar";
+
     private RestTemplate restTemplate = new RestTemplate();
 
     public EmailServiceClient() {
@@ -22,19 +25,33 @@ public class EmailServiceClient {
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         // Construir el cuerpo de la solicitud
-
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         try {
-            ResponseEntity<String> response = restTemplate.postForEntity(SERVER_URL, entity, String.class);
+            // Intentar enviar la solicitud al primer servidor
+            ResponseEntity<String> response = restTemplate.postForEntity(SERVER_URL_1, entity, String.class);
+
+            if (response.getStatusCode() == HttpStatus.CREATED) {
+                System.out.println("Correo enviado correctamente");
+                return; // Salir del método si el correo se envió correctamente
+            } else {
+                System.out.println("Error al enviar el correo desde " + SERVER_URL_1 + ": " + response.getBody());
+            }
+        } catch (HttpClientErrorException e) {
+            System.out.println("Error al enviar el correo desde " + SERVER_URL_1 + ": " + e.getMessage());
+        }
+
+        // Si falla el primer intento, intentar con el segundo servidor
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(SERVER_URL_2, entity, String.class);
 
             if (response.getStatusCode() == HttpStatus.CREATED) {
                 System.out.println("Correo enviado correctamente");
             } else {
-                System.out.println("Error al enviar el correo: " + response.getBody());
+                System.out.println("Error al enviar el correo desde " + SERVER_URL_2 + ": " + response.getBody());
             }
         } catch (HttpClientErrorException e) {
-            System.out.println("Error al enviar el correo: " + e.getMessage());
+            System.out.println("Error al enviar el correo desde " + SERVER_URL_2 + ": " + e.getMessage());
         }
     }
 }
